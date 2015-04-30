50a2af8c46ec2ad9dbdfe7bdd7e6d6c67df08606
noMoon <nomoonx@gmail.com>
2015-04-30 01:24:32 -0400

    hoho

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+
+	<title><?php bloginfo('name'); ?><?php wp_titlle(); ?></title>
+	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
 </head>
 <body>
 	<?php echo bloginfo('url'); ?>
-	<a href=”<?php bloginfo('url'); ?>”><?php bloginfo('name'); ?></a>
+	<a href=<?php bloginfo('url'); ?>><?php bloginfo('name'); ?></a>
 </body>
 </html>
