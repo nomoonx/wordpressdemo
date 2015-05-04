b9ec594f062641f44baf2716ebc9a1d8bacd0554
noMoon <nomoonx@gmail.com>
2015-05-04 16:11:32 -0400

    hoho

---
+++ header.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <title><?php wp_title('|', true, 'right'); ?></title>
+
+    <!-- Mobile Specific Meta -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+    <link rel="profile" href="http://gmpg.org/xfn/11">
+    <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
+
+    <!--[if lt IE 9]>
+    <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js"></script>
+    <![endif]-->
+
+    <?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+
+<header class="top-header">
+
+    <div class="container">
+
+        <div class="row">
+
+            <div class="col-sm-4">
+
+                <div class="header-info">
+                    <h3><a href="<?php echo esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a></h3>
+                </div>
+                <!-- end header-info -->
+
+            </div>
+            <!-- end col-sm-3 -->
+
+            <div class="logo">
+                <a href="<?php echo esc_url( home_url('/')); ?>">
+                    <?php if( of_get_option( 'logo', true ) != '' ) { ; ?>
+                        <?php if( of_get_option('logo',true) == 1 ) { ?>
+                            <h1><?php bloginfo('name'); ?></h1>
+                            <p><?php bloginfo('description'); ?></p>
+                        <?php } else { ?>
+                            <img src="<?php echo esc_url( of_get_option( 'logo', true )); ?>" / >
+                                        <?php } } else { ?>
+                        <h1><?php bloginfo('name'); ?></h1>
+                    <?php } ?>
+                    <p><?php bloginfo('description'); ?></p>
+                </a>
+            </div><!-- logo -->
+
+            <div class="col-sm-8">
+
+                <!-- Custom Top Menu -->
+
+                <?php wp_nav_menu(array(
+                    'theme_location' => 'primary',
+                    'container' => 'nav',
+                    'container_class' => 'col-sm-9 pull-right',
+                    'menu_class' => 'nav navbar-nav',
+                    'fallback_cb' => 'false',
+                    'depth' => 2
+                )); ?>
+
+                <!-- End Custom Top Menu -->
+
+
+            </div>
+            <!-- end col-sm-9 -->
+
+        </div>
+        <!-- end row -->
+
+    </div>
+    <!-- end container -->
+
+</header>
+<!-- end top-header -->
+
+
+<header class="mobile-header">
+
+    <div class="container">
+
+
+        <div class="col-sm-3">
+
+            <div class="header-info">
+                <h3><a href="<?php echo esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a></h3>
+
+                <p><?php bloginfo('description'); ?></p>
+            </div>
+            <!-- end header-info -->
+
+        </div>
+        <!-- end col-sm-3 -->
+
+
+        <div class="fa-bars-header clearfix">
+            <i class="fa fa-bars"></i>
+        </div>
+        <!-- fa-bars-header -->
+
+
+        <div class="mobile-nav">
+
+            <!-- Custom Mobile menu -->
+
+            <?php wp_nav_menu(array(
+                'theme_location' => 'mobile-menu',
+                'container' => 'nav',
+                'container_class' => 'mobile-menu',
+                'menu_class' => 'nav',
+                'fallback_cb' => 'false',
+                'depth' => 2
+            )); ?>
+
+            <!-- End Custom Mobile Menu -->
+
+
+        </div>
+        <!-- end mobile-nav -->
+
+    </div>
+    <!-- end container -->
+
+</header>
+<!-- end mobile-header -->
--- index.php
+++ index.php
@@ -1,14 +1,65 @@
-<!DOCTYPE html>
-<html>
-<head>
+<?php get_header(); ?>
 
-	<title><?php bloginfo('name'); ?></title>
-	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
-</head>
-<body>
-	<?php echo bloginfo('url'); ?>
-	<a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a>
-	 <?php
-    bloginfo(’description’); ?>
-</body>
-</html>
+<?php
+
+if (is_home() && $paged < 2) {
+    get_template_part('slider');
+}
+
+?>
+
+    <section class="section-content-area">
+
+        <div class="container">
+
+            <div class="row">
+
+                <div class="col-sm-9">
+
+                    <div class="posts-area">
+
+                        <?php if (have_posts()): ?>
+
+                            <?php while (have_posts()):the_post(); ?>
+
+                                <?php get_template_part('content', get_post_format()); ?>
+
+                            <?php endwhile; ?>
+
+                            <?php vision_pagenavi(); ?>
+
+                        <?php else: ?>
+
+                            <?php get_template_part('content', 'none'); ?>
+
+                        <?php endif; ?>
+
+                    </div>
+                    <!-- end posts-area -->
+
+                </div>
+                <!-- end col-sm-9 -->
+
+                <div class="col-sm-3">
+
+                    <div class="sidebar">
+
+                        <?php get_sidebar(); ?>
+
+                    </div>
+                    <!-- end sidebar -->
+
+                </div>
+                <!-- end col-sm-3 -->
+
+            </div>
+            <!-- end row -->
+
+        </div>
+        <!-- end container -->
+
+    </section> <!-- end section-content-area -->
+
+<?php get_template_part('footer', 'main'); ?>
+
+<?php get_footer(); ?>
