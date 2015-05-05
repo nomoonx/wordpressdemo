93da963198c92e3aab1410dbe5b583b99e7c64ae
noMoon <nomoonx@gmail.com>
2015-05-04 23:43:39 -0400

    hoho

--- functions.php
+++ functions.php
@@ -11,6 +11,53 @@ load_template( $optionsfile );
  **********************************************************************************/
 require_once(get_template_directory().'/functions/pagination.php');
 
+if(function_exists("register_field_group"))
+{
+    register_field_group(array (
+        'id' => 'acf_slider-options',
+        'title' => 'Slider Options',
+        'fields' => array (
+            array (
+                'key' => 'field_536c7915da76c',
+                'label' => 'Featured Slider',
+                'name' => 'featured_slider',
+                'type' => 'true_false',
+                'instructions' => 'Enable Featured Slider',
+                'message' => 'Enable Slider ?',
+                'default_value' => 0,
+            ),
+            array (
+                'key' => 'field_536c7a626da29',
+                'label' => 'Slider Image',
+                'name' => 'slider_image',
+                'type' => 'image',
+                'instructions' => 'Ideal image\'s height should be 400px.',
+                'save_format' => 'object',
+                'preview_size' => 'thumbnail',
+                'library' => 'all',
+            ),
+        ),
+        'location' => array (
+            array (
+                array (
+                    'param' => 'post_type',
+                    'operator' => '==',
+                    'value' => 'post',
+                    'order_no' => 0,
+                    'group_no' => 0,
+                ),
+            ),
+        ),
+        'options' => array (
+            'position' => 'normal',
+            'layout' => 'default',
+            'hide_on_screen' => array (
+            ),
+        ),
+        'menu_order' => 0,
+    ));
+}
+
 
 function my_theme_register_required_plugins() {
 
