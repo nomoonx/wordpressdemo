09691e0f22ff536c4f50577860b1de935f83776e
noMoon <nomoonx@gmail.com>
2015-05-04 22:59:50 -0400

    hoho

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
-define( 'OPTIONS_FRAMEWORK_DIRECTORY', get_template_directory_uri() . '/plugin/' );
-require_once get_template_directory() . '/plugin/options-framework.php';
+define( 'OPTIONS_FRAMEWORK_DIRECTORY', get_template_directory_uri() . '/inc/' );
+require_once get_template_directory() . '/inc/options-framework.php';
 
 // Loads options.php from child or parent theme
 $optionsfile = locate_template( 'options.php' );
